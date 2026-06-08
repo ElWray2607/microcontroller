bc3a14dda678216c645e3cc10926441bb15a0c85
SchrepfeTo <tom.schrepfer@ireks.com>
2026-06-08 11:14:02 +0200

    Implement gas sensor

---
+++ controller/GasSensorController.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from service.GasSensorService import GasSensorService
+from util.networking import MQTTManager
+
+class GasSensorController:
+    TOPIC_GAS_SENSOR_STATE = "home/gas_sensor/gas"
+
+    def __init__(self, mqtt_client: MQTTManager, gas_sensor_pin_id: int):
+        self.mqtt_client = mqtt_client
+        self.gas_sensor_service = GasSensorService(gas_sensor_pin_id)
+
+    async def run(self):
+        while True:
+            self.mqtt_client.publish(self.TOPIC_GAS_SENSOR_STATE, self.gas_sensor_service.gas_detected())
+            await asyncio.sleep(1)
