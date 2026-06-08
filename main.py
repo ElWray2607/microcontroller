c15caaefb561245d8a5b97ad4dc7df2efa179871
SchrepfeTo <tom.schrepfer@ireks.com>
2026-06-08 11:04:52 +0200

    Implement networking logic

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import asyncio
 
+from util.networking import NetworkManager, MQTTManager
+
+WIFI_SSID = "Schueler-Mobil"
+WIFI_PASSWORD = "nfSuLITC"
+
+CLIENT_ID = "HOMECONTROL"
+BROKER_IP = "192.168.8.100"
+
+async def init_wifi():
+    network_manager = NetworkManager(WIFI_SSID, WIFI_PASSWORD)
+    await network_manager.connect()
+
+    if network_manager.is_connected():
+        print("WiFi status:")
+        print(network_manager.status())
+
+    return network_manager
+
+async def init_mqtt():
+    mqtt_manager = MQTTManager(CLIENT_ID, BROKER_IP)
+    await mqtt_manager.connect()
+
+    return mqtt_manager
 
 async def main():
-    pass
+    network_manager = await init_wifi()
+    mqtt_manager = await init_mqtt()
+
+
+    while True:
+        await asyncio.sleep(1)
 
 asyncio.run(main())
